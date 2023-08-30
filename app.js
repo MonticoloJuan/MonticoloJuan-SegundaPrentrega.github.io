90aa5a226692d6b84d0d4728f30b8e1aee738477
Monticolo Juan Ignacio <juan.monticolo@gmail.com>
2023-08-29 22:54:17 -0300

    v3

--- app.js
+++ app.js
@@ -34,11 +34,16 @@ alert("Bienvenido a la calculadora de Promedios")
 
 do {
     let materia = prompt("Dinos cual es la materia que quieres sacar el promedio")
-    let nota1 = Number(prompt("Ingrese su primer nota"))
-    let nota2 = Number(prompt("Ingrese su segunda nota"))
-    let nota3 = Number(prompt("Ingrese su tercer nota"))
-    const materias = new Promedio (materia, nota1, nota2, nota3)
-    listadematerias.agregarmaterias(materias)
+    let nota1 = Number(prompt("Ingrese su primer nota entre 1 y 10"))
+    let nota2 = Number(prompt("Ingrese su segunda nota entre 1 y 10"))
+    let nota3 = Number(prompt("Ingrese su tercer nota entre 1 y 10"))
+    if ((nota1 <= 10)&&(nota2<=10)&&(nota3<=10)&&(materia!=="")){
+        const materias = new Promedio (materia, nota1, nota2, nota3)
+        listadematerias.agregarmaterias(materias)
+    } else {
+        alert("Ah ingresado notas invalidas o no ha ingresado una materia")
+    }
+    
 
     continuar = confirm("¿Quiere sacar promedios de alguna otra materia?")
 } while (continuar);
