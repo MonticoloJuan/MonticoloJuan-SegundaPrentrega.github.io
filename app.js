74d1f844a7dcfa9bcbb9ddc9d144c7446cec4d4c
Monticolo Juan Ignacio <juan.monticolo@gmail.com>
2023-08-30 00:10:39 -0300

    v.3.1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ class Promedio {
     }
 
     descripcionmateria(){
-        let promedionotas = (this.nota1+this.nota2+this.nota3) /3
+        let promedionotas = Math.round ((this.nota1+this.nota2+this.nota3) /3)
         return `En la Materia ${this.materia} tiene las siguientes notas:\n Primer nota: ${this.nota1}\n Segunda nota: ${this.nota2}\n Tercer nota: ${this.nota3}\n Con el promedio de: ${promedionotas}`
     }
 }
